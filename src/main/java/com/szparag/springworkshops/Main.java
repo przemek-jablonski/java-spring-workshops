0ca797371dbaeda206ed92b1c122c0aa0e3ae01d
Przemysław Jabłoński <sharaquss@gmail.com>
2016-02-20 09:49:41 +0100

    importing project as gradle, launch from terminal

--- src/main/java/com/szparag/springworkshops/Main.java
+++ src/main/java/com/szparag/springworkshops/Main.java
@@ -3,9 +3,11 @@ package com.szparag.springworkshops;
 /**
  * Created by Ciemek on 20/02/16.
  */
-public class main {
+public class Main {
+
 
     public static void main(String[] args) {
         System.out.println("Hello from package");
     }
+
 }
