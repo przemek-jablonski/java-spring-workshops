810112125503316e00c01232503d20ea3a160e75
Przemysław Jabłoński <sharaquss@gmail.com>
2016-02-20 11:01:47 +0100

    bean can be launched from terminal and java -jar

--- src/main/java/com/szparag/springworkshops/BaseController.java
+++ src/main/java/com/szparag/springworkshops/BaseController.java
@@ -1,7 +1,20 @@
 package com.szparag.springworkshops;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by Ciemek on 20/02/16.
  */
+@Controller
+@EnableAutoConfiguration
 public class BaseController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World(spring)(baseController)";
+    }
 }
--- src/main/java/com/szparag/springworkshops/Main.java
+++ src/main/java/com/szparag/springworkshops/Main.java
@@ -1,5 +1,8 @@
 package com.szparag.springworkshops;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 /**
  * Created by Ciemek on 20/02/16.
  */
@@ -8,6 +11,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello from package");
+//        SpringApplication.run(BaseController.class, args);
+        SpringApplication.run(BaseController.class, args);
     }
 
 }
