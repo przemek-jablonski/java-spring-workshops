bdbfc130ca6cd0d9590f502051d1cd864495eee5
Przemysław Jabłoński <sharaquss@gmail.com>
2016-02-20 09:12:36 +0100

    init

---
+++ src/com/szparag/springworkshops/main.java
@@ -0,0 +1,11 @@
+package com.szparag.springworkshops;
+
+/**
+ * Created by Ciemek on 20/02/16.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from package");
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ciemek on 20/02/16.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello worldrld");
+    }
+
+}
